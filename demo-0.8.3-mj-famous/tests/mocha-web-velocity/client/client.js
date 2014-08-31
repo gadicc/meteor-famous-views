b835fa514400d72153c4eb7f75aa8f163312aabb
Gadi Cohen <dragon@wastelands.net>
2014-08-31 18:32:34 +0300

    change test wrapping

--- demo-0.8.3-mj-famous/tests/mocha-web-velocity/client/client.js
+++ demo-0.8.3-mj-famous/tests/mocha-web-velocity/client/client.js
@@ -16,26 +16,27 @@ var wrap2 = function(func) {
 }
 
 if (!(typeof MochaWeb === 'undefined')){
-	MochaWeb.testOnly(function() {
+	FView.ready(function() {
 
-		describe("Data Context tests", function() {
+		MochaWeb.testOnly(function() {
 
-			/*
-			it("should fail after 1s", function(done) {
-				window.setTimeout(function() {
-					// this times out (actually, no code below chai is executed)
-					chai.assert.equal(true, false);
-					done();
+			describe("Data Context tests", function() {
 
-					// this passes (on mirror JS console, I see the failure)
-					done();
-					chai.assert.equal(true, false);
-				}, 1000);
-			});
-			*/
+				/*
+				it("should fail after 1s", function(done) {
+					window.setTimeout(function() {
+						// this times out (actually, no code below chai is executed)
+						chai.assert.equal(true, false);
+						done();
 
-			it("View with args must return parent data context", function(done) {
-				FView.ready(function() {
+						// this passes (on mirror JS console, I see the failure)
+						done();
+						chai.assert.equal(true, false);
+					}, 1000);
+				});
+				*/
+
+				it("View with args must return parent data context", function(done) {
 					Template.dataContext_viewWithArgs.myData = { a: 1 };
 					Template.dataContext_viewWithArgs.getData = wrap(function() {
 						chai.assert.propertyVal(this, 'a', 1);
@@ -43,10 +44,8 @@ if (!(typeof MochaWeb === 'undefined')){
 					});
 					Blaze.render(Template.dataContext_viewWithArgs);
 				});
-			});
 
-			it("View with no args must return same data context (#45) (1/2)", function(done) {
-				FView.ready(function() {
+				it("View with no args must return same data context (#45) (1/2)", function(done) {
 					Template.dataContext_viewWithoutArgs.myData = { a: 1 };
 					Template.dataContext_viewWithoutArgs.getData = wrap(function() {
 						chai.assert.propertyVal(this, 'a', 1);
@@ -54,10 +53,8 @@ if (!(typeof MochaWeb === 'undefined')){
 					});
 					Blaze.render(Template.dataContext_viewWithoutArgs);
 				});
-			});
 
-			it("View with no args must return same data context (#45) (2/2)", function(done) {
-				FView.ready(function() {
+				it("View with no args must return same data context (#45) (2/2)", function(done) {
 					Template.dataContext_viewWithoutArgs2.myData = { a: 1 };
 					Template.dataContext_viewWithoutArgs2_helper.getData = wrap(function() {
 						chai.assert.propertyVal(this, 'a', 1);
@@ -65,10 +62,8 @@ if (!(typeof MochaWeb === 'undefined')){
 					});
 					Blaze.render(Template.dataContext_viewWithoutArgs2);
 				});
-			});
 
-			it("If data= as dataContext if specified (#46)", function(done) {
-				FView.ready(function() {
+				it("If data= as dataContext if specified (#46)", function(done) {
 					Template.dataContext_explicit.myData = { a: 1 };
 					Template.dataContext_explicit.getData = wrap(function() {
 						chai.assert.propertyVal(this, 'a', 1);
@@ -76,24 +71,21 @@ if (!(typeof MochaWeb === 'undefined')){
 					});
 					Blaze.render(Template.dataContext_explicit);
 				});
-			});
 
-		});  /* Data Context tests */
+			});  /* Data Context tests */
 
-		describe("Other tests", function() {
+			describe("Other tests", function() {
 
-			it("Views should work if inside a template with a space in the name", function(done) {
-				FView.ready(function() {
+				it("Views should work if inside a template with a space in the name", function(done) {
 					Template["space template"].success = wrap(function() {
 						done();
 					});
 					// We can't catch the error directly, so will pass first
 					Blaze.render(Template["space template"]);
 				});
-			});
 
-		});  /* Other tests */
+			});  /* Other tests */
 
-
-	});
-}
+		}); /* MochaWeb.testOnly */
+	}); /* FView.ready */
+} /* MochaWeb defined */
