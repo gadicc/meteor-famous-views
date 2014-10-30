fc966f5dd43af94d69a8ea67fc27be5661ae37c9
Gadi Cohen <dragon@wastelands.net>
2014-10-30 12:57:15 +0200

    Quick hack for {{#Modifier}} support (issue #110)

---
+++ demo-base/client/issues/issue110.js
@@ -0,0 +1,9 @@
+Menu.add({name:'#110 modifier view',route:'issues/110'}, 'Issues');
+
+Router.map(function() {
+  this.route('issue110', { path: 'issues/110' } );
+});
+
+Template.issue110.getSize = function() {
+	return Session.get('getSize') || [undefined,160];
+}
