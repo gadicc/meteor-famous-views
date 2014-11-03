88f137648a1845bc524c47431c5234bf686364ef
Gadi Cohen <dragon@wastelands.net>
2014-11-03 18:36:45 +0200

    docs (closes #88, closes #123, closes #118, closes #115)

--- demo-base/client/features/_features.js
+++ demo-base/client/features/_features.js
@@ -1,9 +1,7 @@
 Menu.add({name:'Iron Router',route:'features/iron-router'}, 'Features');
 Menu.add({name:'Reactivity',route:'features/reactivity'}, 'Features');
+Menu.add({name:'API Docs',route:'features/api'}, 'Features');
 
 // Static pages don't need their own JS
-Router.map(function() {
-  this.route('features_ironRouter', {
-  	path: '/features/iron-router'
-  });
-});
+Router.route('features_ironRouter', { path: '/features/iron-router' });
+Router.route('features_api', { path: '/features/api' });
