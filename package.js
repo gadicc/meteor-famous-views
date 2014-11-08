07cb469edf838c2b52ffe64504c3be16a2aaf694
Gadi Cohen <dragon@wastelands.net>
2014-11-08 15:57:59 +0200

    v0.1.22; yet to be documented cool stuff (see History) bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "gadicohen:famous-views",
   summary: 'Blaze Views for Famous; doing Famous Meteor-style',
-  version: "0.1.21",
+  version: "0.1.22",
   git: "https://github.com/gadicc/meteor-famous-views.git"
 });
 
