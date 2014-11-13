792b4a20f5f5a65eaf51111ac62cf0ff2adbb2fc
Gadi Cohen <dragon@wastelands.net>
2014-11-13 19:14:51 +0200

    stash initial datacontext work

--- tests/famous.js
+++ tests/famous.js
@@ -1,31 +1,23 @@
-Tinytest.add('famous-views - famous.js - famousEvents', function(test) {
-
-	/* Preparation */
+Tinytest.addAsync('famous-views - famous.js - famousEvents', function(test, complete) {
 
 	var div = document.createElement('div');
-
-	Template.tests_famous_surface.rendered = function() {
-	};
-
-	// famousEvents
 	var famousEvents = {};
+
 	Template.tests_famous_surface.famousEvents({
 		'a': function() { famousEvents.a = 'a' },
 		'b': function() { famousEvents.b = 'b' }
 	});
 
+	Template.tests_famous_surface.rendered = function() {
+		var events = FView.from(this).surface._eventOutput.listeners;
+		events.a[0]();
+		test.equal(famousEvents, {a:'a'});
+		events.b[0]();
+		test.equal(famousEvents, {a:'a',b:'b'});
+		complete();
+	};
+
 	Blaze.render(Template.tests_famous, div);
-	var surface_fview = FView.byId("tests_famous_surface");
-
-	/* Tests */
-
-	// famousEvents
-	var events = surface_fview.surface._eventOutput.listeners;
-	events.a[0]();
-	test.equal(famousEvents, {a:'a'});
-	events.b[0]();
-	test.equal(famousEvents, {a:'a',b:'b'});
-
 });
 
 /* --- data context tests and helper functions --- */
@@ -37,6 +29,7 @@ function dataTest(tNum, test, complete) {
 	var div = document.createElement('div');
 
 	Template.tests_famous_dataContextCheck.rendered = function() {
+		console.log(this);
 		test.equal(this.data, myData);
 		complete();
 	}
