545a1474892296b80a1bb372c4f4206d2e28acc7
Gadi Cohen <dragon@wastelands.net>
2014-11-13 12:42:09 +0200

    plugin page

--- demo-base/client/more/more.js
+++ demo-base/client/more/more.js
@@ -1,13 +1,11 @@
-Menu.add({name:'GitHub',route:'https://github.com/gadicc/meteor-famous-components'}, 'More');
-Menu.add({name:'Testimonials',route:'more/testimonials'}, 'More');
 Menu.add({name:'FAQ',route:'faq'}, 'More');
+Menu.add({name:'Plugins',route:'plugins'}, 'More');
+Menu.add({name:'Testimonials',route:'more/testimonials'}, 'More');
+Menu.add({name:'GitHub',route:'https://github.com/gadicc/meteor-famous-components'}, 'More');
 
 // Static pages don't need their own JS
-Router.map(function() {
-  this.route('more_testimonials', {
-  	path: '/more/testimonials'
-  });
-  this.route('faq', {
-  	path: '/faq'
-  });
+Router.route('more_testimonials', {
+	path: '/more/testimonials'
 });
+Router.route('faq');
+Router.route('plugins');
---
+++ demo-base/client/more/plugins.js
@@ -0,0 +1,26 @@
+Blaze.registerHelper('plugin', new Blaze.Template(function() {
+	var view = this;
+
+  if (!view.templateContentBlock)
+  	return 'No content';
+
+  var content = Blaze._toText(view.templateContentBlock, HTML.TEXTMODE.STRING);
+
+  // Remove initial newlines and initial indent
+	content = content.replace(/^\n*/, '');
+	var initialIndent = content.match(/^([\t ]*)/);
+	content = content.replace(new RegExp('^' + initialIndent[0], 'gm'), '');
+
+	var match, data={}, re=/^([A-Za-z]+)=(.*)$/gm, last;
+	while((match = re.exec(content)) !== null) {
+		data[match[1]] = match[2];
+		last = match;
+	}
+	data.desc = content.substr(last.index + last[0].length + 1 /* \n */);
+	data.desc = marked(data.desc);
+
+	data.name = data.name.replace(/fview-(\w+)/,
+		'fview-<span class="pluginNameMain">$1</span')
+
+	return Blaze.With(data, function() { return Template.plugin_template; });
+}));
