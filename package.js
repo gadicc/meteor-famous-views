f98056620fbd866566f3f96d22d3da0ba983ed50
Gadi Cohen <dragon@wastelands.net>
2014-11-17 14:26:15 +0200

    v0.1.27-pre.2; final CSS classs tweaks, trigger resize (#138)

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "gadicohen:famous-views",
   summary: 'Blaze Views for Famous; doing Famous Meteor-style',
-  version: "0.1.27-pre.1",
+  version: "0.1.27-pre.2",
   git: "https://github.com/gadicc/meteor-famous-views.git"
 });
 
