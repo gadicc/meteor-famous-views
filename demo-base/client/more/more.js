994327240d1f5d3b621426347b46e6ff9aa361bc
Gadi Cohen <dragon@wastelands.net>
2015-05-14 12:00:18 +0200

    Performance page

--- demo-base/client/more/more.js
+++ demo-base/client/more/more.js
@@ -4,9 +4,14 @@ Menu.add({name:'Plugins',route:'plugins'}, 'More');
 Menu.add({name:'GitHub',route:'https://github.com/gadicc/meteor-famous-components'}, 'More');
 Menu.add({name:'Changelog',route:'https://github.com/gadicc/meteor-famous-views/blob/master/History.md'}, 'More');
 Menu.add({name:'Testimonials',route:'more/testimonials'}, 'More');
+Menu.add({name:'Performance',route:'performance'}, 'More');
 
 // Static pages don't need their own JS
+
 Router.route('more_testimonials', {
   path: '/more/testimonials'
 });
+
 Router.route('faq');
+
+Router.route('performance');
