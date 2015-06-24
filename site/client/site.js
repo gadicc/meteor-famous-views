597ef6bfbf1ea4969481dd4fe5754680662236d9
Gadi Cohen <dragon@wastelands.net>
2015-06-24 22:38:26 +0100

    add site dir

---
+++ site/client/site.js
@@ -0,0 +1,69 @@
+
+Session.setDefault('name', 'Gadi');
+Session.setDefault('size', 'A:15,R:10,D:5');
+Session.setDefault('rotation', [0,1]);
+
+Items = new Mongo.Collection(null);
+Items.insert({name: 'a'});
+Items.insert({name: 'b'});
+Items.insert({name: 'c'});
+
+Template.body.helpers({
+  x: function() {
+    return Session.get('x');
+  },
+  items: function() {
+    return Items.find();
+  },
+  /*
+  name: function() {
+    console.log('nameHelper');
+//    FView.current().updateSizeDeferred();
+    return Session.get('name');
+  },
+  */
+  getRotation: function() {
+    return Session.get('rotation');
+  },
+  getSize: function() {
+    return Session.get('size');
+  },
+  geta: function() {
+    return Session.set('a');
+  },
+  onRender: function() {
+//    console.log('or', this);
+  //  console.log(FView.current());
+    var fview = FView.current();
+    console.log(fview);
+    var clock = famous.core.FamousEngine.getClock();
+    clock.setInterval(function() {
+          var time = clock.getTime();
+
+          fview.node.setRotation(
+              time / 1500,
+              time / 1200,
+              time / 1300
+          );
+
+      }, 16);
+
+//    fview.position = new famous.components.Position(fview.node);
+//    fview.position.set(150, 150, 0, { duration: 1000, method: 'spring' });
+
+  }
+});
+
+Template.tplX.events({
+  'click': function(event, tpl) {
+    console.log(FView.current());
+    console.log(FView.from(tpl));
+  }
+});
+
+Template.tplX.helpers({
+  'yo': function() {
+    console.log(this);
+    return 'yo';
+  }
+});
