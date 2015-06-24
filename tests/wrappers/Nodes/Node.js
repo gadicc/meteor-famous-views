874fa8cc41f5dfc909701ed2e4b52522115e73e7
Gadi Cohen <dragon@wastelands.net>
2015-06-24 10:02:00 +0100

    stash unfinished work pending move to new branch

--- tests/wrappers/Nodes/Node.js
+++ tests/wrappers/Nodes/Node.js
@@ -1,3 +1,28 @@
+// https://github.com/Famous/engine/issues/329
+
+var SizeSystem = famous.core.SizeSystem;
+famous.components.Size.prototype.getValue = function() {
+  var ss = SizeSystem.get(this._node.getLocation());
+  return {
+    sizeMode: ss && ss.getSizeMode(),
+    absolute: {
+      x: this._absolute.x.get(),
+      y: this._absolute.y.get(),
+      z: this._absolute.z.get()
+    },
+    differential: {
+      x: this._differential.x.get(),
+      y: this._differential.y.get(),
+      z: this._differential.z.get()
+    },
+    proportional: {
+      x: this._proportional.x.get(),
+      y: this._proportional.y.get(),
+      z: this._proportional.z.get()
+    }
+  };
+};
+
 function fviewWithNode(source) {
   var fview = new FView._classes._Node(null, null, source);
   fview.node = new famous.core.Node();
@@ -5,6 +30,7 @@ function fviewWithNode(source) {
 }
 
 var Node = famous.core.Node;
+var SizeSystem = famous.core.SizeSystem;
 
 Tinytest.add('famous-views - Wrappers - Node - size component', function(test) {
   var fview = fviewWithNode('wrappers/node/sizeComponent');
@@ -12,8 +38,10 @@ Tinytest.add('famous-views - Wrappers - Node - size component', function(test) {
   fview.attrUpdate('size', "A:100; renderSize; relative: 2,10");
   var result = fview.size.getValue();
   // ok i'm doing something wrong or this is a bug :)
+  // probably because no context set on unmounted node
   if (!result.sizeMode)
     result.sizeMode = fview.node.getSizeMode();
+
   test.equal(result.sizeMode, { 0:Node.ABSOLUTE_SIZE, 1:Node.RENDER_SIZE, 2:Node.RELATIVE_SIZE }, 'modes from A:,renderSize,R:');
   test.equal(result.absolute.x, 100, 'setAbsolute');
   test.equal(result.proportional.z, 2, 'setProportional via R');
@@ -21,7 +49,6 @@ Tinytest.add('famous-views - Wrappers - Node - size component', function(test) {
 
   fview.attrUpdate('size', "P:5; D:-10");
   var result = fview.size.getValue();
-  // ok i'm doing something wrong or this is a bug :)
   if (!result.sizeMode)
     result.sizeMode = fview.node.getSizeMode();
   test.equal(result.sizeMode, { 0:Node.RELATIVE_SIZE, 1:Node.RELATIVE_SIZE, 2:Node.DEFAULT_SIZE });
@@ -32,7 +59,6 @@ Tinytest.add('famous-views - Wrappers - Node - size component', function(test) {
 
   fview.attrUpdate('size', "Prop:5; Diff:-10; RenderSize");
   var result = fview.size.getValue();
-  // ok i'm doing something wrong or this is a bug :)
   if (!result.sizeMode)
     result.sizeMode = fview.node.getSizeMode();
   test.equal(result.sizeMode, { 0:Node.RELATIVE_SIZE, 1:Node.RELATIVE_SIZE, 2:Node.RENDER_SIZE });
@@ -66,6 +92,7 @@ Tinytest.addAsync('famous-views - Wrappers - Node - _onRender', function(test, c
   });
 
   Blaze.render(Template.node2, testDiv());
+  Tracker.flush();
 });
 
 // also tests: addToParent, addChild
@@ -79,6 +106,7 @@ Tinytest.addAsync('famous-views - Wrappers - Node - template create', function(t
     complete();
   };
   Blaze.render(Template.node3, testDiv());
+  Tracker.flush();
 });
 
 // also tests: dismount
@@ -97,7 +125,12 @@ Tinytest.addAsync('famous-views - Wrappers - Node - template destroy', function(
       test.equal(scene.node._children.indexOf(node.node), -1);
       complete();
     });
-    x.set(false);
+
+    _.defer(function() {
+      x.set(false);
+      Tracker.flush();
+    });
   };
   Blaze.render(Template.node4, testDiv());
+  Tracker.flush();
 });
